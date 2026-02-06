21098f6517fd96afd56508b1d6f20f1873a55ce7
Paco De Luis <ponchoflores464@gmail.com>
2026-02-06 10:11:28 -0600

    04

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,22 +1,20 @@
 import axios from "axios";
+import { API_URL } from "../env";
+
 
 const api = axios.create({
-    baseURL: "https://hippiatrical-johnathon-vixenly.ngrok-free.dev/api/",
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("access");
-
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("access");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default api;
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,11 @@ declare module "*.png";
 declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.svg";
+
+/// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly REACT_APP_API_URL: string;
+  }
+}
